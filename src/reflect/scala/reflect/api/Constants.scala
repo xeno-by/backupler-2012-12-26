13c4ad68d209e74085aec13b8196e2abdb53093f
Eugene Burmako <xeno.by@gmail.com>
2012-10-01 16:07:30 +0200

    docs for scala.reflect.api.Constants

--- src/reflect/scala/reflect/api/Constants.scala
+++ src/reflect/scala/reflect/api/Constants.scala
@@ -6,10 +6,82 @@
 package scala.reflect
 package api
 
-/**
- * Defines the type hierachy for compile-time constants.
+/** A slice of the Scala reflection cake that defines compile-time constants and operations on them.
  *
- * @see [[scala.reflect]] for a description on how the class hierarchy is encoded here.
+ *  According to the section 6.24 "Constant Expressions" of the Scala language specification,
+ *  certain expressions (dubbed ''constant expressions'') can be evaluated by the Scala compiler at compile-time.
+ *
+ *  [[scala.reflect.api.Constants#Constant]] instances represent certain kinds of these expressions
+ *  (with values stored in the `value` field and its strongly-typed views named `booleanValue`, `intValue` etc.), namely:
+ *    1. Literals of primitive value classes (bytes, shorts, ints, longs, floats, doubles, chars, booleans and voids).
+ *    1. String literals.
+ *    1. References to classes (typically constructed with [[scala.Predef#classOf]]).
+ *    1. References to enumeration values.
+ *
+ *  Such constants are used to represent literals in abstract syntax trees (the [[scala.reflect.api.Trees#Literal]] node)
+ *  and literal arguments for Java class file annotations (the [[scala.reflect.api.Annotations#LiteralArgument]] class).
+ *
+ *  === Representation of constants ===
+ *
+ *  The `value` field deserves some explanation. Primitive and string values are represented as themselves, whereas
+ *  references to classes and enums are a bit roundabout.
+ *
+ *  Class references are represented as instances of [[scala.reflect.api.Types#Type]]
+ *  (because when the Scala compiler processes a class reference, the underlying runtime class might not yet have been compiled).
+ *  To convert such a reference to a runtime class, one should use the `runtimeClass` method of a mirror such as [[scala.reflect.api.Mirrors#RuntimeMirror]]
+ *  (the simplest way to get such a mirror is using [[scala.reflect.api.package#currentMirror]]).
+ *
+ *  Enumeration value references are represented as instances of [[scala.reflect.api.Symbols#Symbol]], which on JVM point to methods
+ *  that return underlying enum values. To inspect an underlying enumeration or to get runtime value of a reference to an enum,
+ *  one should use a [[scala.reflect.api.Mirrors#RuntimeMirror]] (the simplest way to get such a mirror is again [[scala.reflect.api.package#currentMirror]]).
+ *
+ *  === Example ===
+ *  {{{
+ *  enum JavaSimpleEnumeration { FOO, BAR }
+ *
+ *  import java.lang.annotation.*;
+ *  @Retention(RetentionPolicy.RUNTIME)
+ *  @Target({ElementType.TYPE})
+ *  public @interface JavaSimpleAnnotation {
+ *    Class<?> classRef();
+ *    JavaSimpleEnumeration enumRef();
+ *  }
+ *
+ *  @JavaSimpleAnnotation(
+ *    classRef = JavaAnnottee.class,
+ *    enumRef = JavaSimpleEnumeration.BAR
+ *  )
+ *  public class JavaAnnottee {}
+ *  }}}
+ *  {{{
+ *  import scala.reflect.runtime.universe._
+ *  import scala.reflect.runtime.{currentMirror => cm}
+ *
+ *  object Test extends App {
+ *    val jann = typeOf[JavaAnnottee].typeSymbol.annotations(0).javaArgs
+ *    def jarg(name: String) = jann(newTermName(name)).asInstanceOf[LiteralArgument].value
+ *
+ *    val classRef = jarg("classRef").typeValue
+ *    println(showRaw(classRef))             // TypeRef(ThisType(<empty>), JavaAnnottee, List())
+ *    println(cm.runtimeClass(classRef))     // class JavaAnnottee
+ *
+ *    val enumRef = jarg("enumRef").symbolValue
+ *    println(enumRef)                       // value BAR
+ *
+ *    val siblings = enumRef.owner.typeSignature.declarations
+ *    val enumValues = siblings.filter(sym => sym.isVal && sym.isPublic)
+ *    println(enumValues)                    // Scope{
+ *                                           //   final val FOO: JavaSimpleEnumeration;
+ *                                           //   final val BAR: JavaSimpleEnumeration
+ *                                           // }
+ *
+ *    // doesn't work because of https://issues.scala-lang.org/browse/SI-6459
+ *    // val enumValue = mirror.reflectField(enumRef.asTerm).get
+ *    val enumClass = cm.runtimeClass(enumRef.owner.asClass)
+ *    val enumValue = enumClass.getDeclaredField(enumRef.name.toString).get(null)
+ *    println(enumValue)                     // BAR
+ *  }
+ *  }}}
  */
 trait Constants {
   self: Universe =>
@@ -35,22 +107,65 @@ trait Constants {
   }
 
   abstract class ConstantApi {
+    /** Payload of the constant. */
     val value: Any
+
+    /** Scala type that describes the constant. */
     def tpe: Type
-    def isNaN: Boolean
 
+    /** Strongly-typed payload of the constant if the constant represents a boolean.
+     *  Throws an error otherwise.
+     */
     def booleanValue: Boolean
-    def byteValue: Byte
-    def shortValue: Short
-    def charValue: Char
-    def intValue: Int
-    def longValue: Long
-    def floatValue: Float
-    def doubleValue: Double
-    def stringValue: String
-    def typeValue: Type
-    def symbolValue: Symbol
 
-    def convertTo(pt: Type): Constant
+    /** Strongly-typed payload of the constant if the constant represents a byte.
+     *  Throws an error otherwise.
+     */
+    def byteValue: Byte
+
+    /** Strongly-typed payload of the constant if the constant represents a short.
+     *  Throws an error otherwise.
+     */
+    def shortValue: Short
+
+    /** Strongly-typed payload of the constant if the constant represents a char.
+     *  Throws an error otherwise.
+     */
+    def charValue: Char
+
+    /** Strongly-typed payload of the constant if the constant represents an integer.
+     *  Throws an error otherwise.
+     */
+    def intValue: Int
+
+    /** Strongly-typed payload of the constant if the constant represents a long.
+     *  Throws an error otherwise.
+     */
+    def longValue: Long
+
+    /** Strongly-typed payload of the constant if the constant represents a float.
+     *  Throws an error otherwise.
+     */
+    def floatValue: Float
+
+    /** Strongly-typed payload of the constant if the constant represents a double.
+     *  Throws an error otherwise.
+     */
+    def doubleValue: Double
+
+    /** Strongly-typed payload of the constant if the constant represents a string.
+     *  Throws an error otherwise.
+     */
+    def stringValue: String
+
+    /** Strongly-typed payload of the constant if the constant represents a reference to a class.
+     *  Throws an error otherwise.
+     */
+    def typeValue: Type
+
+    /** Strongly-typed payload of the constant if the constant represents a reference to an enum value.
+     *  Throws an error otherwise.
+     */
+    def symbolValue: Symbol
   }
 }
