62bbe072f495fcf960498309ac7762a5fb9a13d1
Eugene Burmako <xeno.by@gmail.com>
2012-12-18 14:54:09 +0100

    adds Tree.nonEmpty

--- src/reflect/scala/reflect/internal/Trees.scala
+++ src/reflect/scala/reflect/internal/Trees.scala
@@ -36,6 +36,8 @@ trait Trees extends api.Trees { self: SymbolTable =>
     def isDef = false
 
     def isEmpty = false
+    def nonEmpty = !isEmpty
+
     def canHaveAttrs = true
 
     /** The canonical way to test if a Tree represents a term.
