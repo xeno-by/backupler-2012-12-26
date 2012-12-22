d1fcf72993e8cc7a436fea484faae755b5ebc6f6
Eugene Burmako <xeno.by@gmail.com>
2012-12-22 17:34:02 +0100

    type macro example now uses c.fresh

--- test/files/run/macro-typemacros-fourth-proto/Impls_Macros_2.scala
+++ test/files/run/macro-typemacros-fourth-proto/Impls_Macros_2.scala
@@ -75,7 +75,7 @@ object Macros {
       }
     }
 
-    val name = TypeName(c.enclosingImpl.name + "_Generated")
+    val name = c.fresh(c.enclosingImpl.name).toTypeName
     c.introduceTopLevel(ClassDef(NoMods, name, Nil, Template(parents, self, body ++ generateCodeForTables())))
     Ident(name)
   }
